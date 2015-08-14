7fde70543c55eb0fb5357e2f090d078674aa6ddd
anlin-yang <anlin0414@outlook.com>
2015-08-14 23:05:55 +0800

    完成了排序的数据迁移到后台，并且不再是export，而是改为了json文件，并动态读取方式加载。

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var hbs = require('hbs');
 var fs = require('fs');
-var sort = require('./sort-score.js');
-var score = require('./scorelist');
+var sortScore = require('./sort-score.js');
 var app = express();
 
-var socreList = score.getscores();
 
 app.use(express.static('bower_components/'));
 app.use(express.static('public/'));
@@ -14,26 +12,31 @@ app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 
 app.get("/", function(req, res) {
-  res.render('index', {
-    scoreList: socreList
+  fs.readFile(__dirname + "/scorelist.json", {
+    encoding: 'utf-8',
+    flag: 'r'
+  }, function(err, data) {
+    if (err) throw err;
+    var scoreList = JSON.parse(data);
+    res.render('index', {
+      scoreList: scoreList
+    });
   });
 });
 
 app.get("/score", function(req, res) {
   var sortKey = req.query.sortKey;
   var sortOrder = req.query.sortOrder;
-  var sortedList = sort(socreList, sortKey, sortOrder);
-  res.send(sortedList);
-  // fs.readFile(__dirname + "/scorelist.js", {
-  //   encoding: 'utf-8',
-  //   flag: 'r'
-  // }, function(err, data) {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //
-  //   res.send(data);
-  // });
+
+  fs.readFile(__dirname + "/scorelist.json", {
+    encoding: 'utf-8',
+    flag: 'r'
+  }, function(err, data) {
+    if (err) throw err;
+    var scoreList = JSON.parse(data);
+    var sortedList = sortScore(scoreList, sortKey, sortOrder);
+    res.send(sortedList);
+  });
 });
 
 app.listen(3000);
--- public/main.js
+++ public/main.js
@@ -1,30 +1,16 @@
 $(function() {
-  var scoreList = getScoreList();
 
   $("#table-stripe thead").on("click", "th", function() {
     if ($(this).data('name')) {
-      // var sortedList = sortScore($(this).data('name'));
-      // if ($(this).data('positiveSeq') === 'Flase') {
-      //   sortedList.reverse();
-      //   $(this).data('positiveSeq', 'True');
-      // } else {
-      //   $(this).data('positiveSeq', 'Flase');
-      // }
-      var sortedList = [];
-      var sortKeyValue = $(this).data('name');
-      var sortOrderValue = $(this).data('order');
       var sortInfo = {
-        sortKey: sortKeyValue,
-        sortOrder: sortOrderValue
+        sortKey: $(this).data('name'),
+        sortOrder: $(this).data('order')
       };
-      if ($(this).data('order') === '-1') {
-        $(this).data('order', '1');
-      } else {
-        $(this).data('order', '-1');
-      }
-      $.get("/score", sortInfo, function(score) {
-        console.log(score);
-        sortedList = score;
+
+      var order = $(this).data('order') === '-1' ? '1' : '-1';
+      $(this).data('order', order);
+
+      $.get("/score", sortInfo, function(sortedList) {
         $("#table-stripe tbody").empty();
         sortedList.forEach(function(val) {
           $("#table-stripe tbody").append("<tr>" +
@@ -35,17 +21,4 @@ $(function() {
       });
     }
   });
-
-  function getScoreList() {
-    var scoreList = [];
-    $(".table-stripe tbody tr").each(function(i) {
-      var tdList = $("td", this);
-      scoreList.push({
-        "name": tdList.eq(0).text().trim(),
-        "chinese": tdList.eq(1).text().trim(),
-        "math": tdList.eq(2).text().trim()
-      });
-    });
-    return scoreList;
-  }
 });
